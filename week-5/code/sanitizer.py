2c03d007c1e873f44b99dde56cfaa72192a5af09
Yuviman <62415750+Yuviman@users.noreply.github.com>
2025-11-01 09:06:06 +0530

    Implement text sanitization function

--- week-5/code/sanitizer.py
+++ week-5/code/sanitizer.py
@@ -1,1 +1,12 @@
+# code/sanitizer.py
+import re, unicodedata
+
+def sanitize_prompt(text):
+    text = re.sub(r'<[^>]+>', ' ', text)
+    text = ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'C')
+    leet_map = str.maketrans("43015", "aeoist")
+    text = text.translate(leet_map)
+    text = re.sub(r'(?i)(ignore (all )?(previous )?instructions|you are now|do anything now|act as)', '', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
 
