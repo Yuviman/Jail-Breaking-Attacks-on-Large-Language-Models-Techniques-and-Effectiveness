b4672056f8a13cae5a895c9d64f8338b3335e9d4
Yuviman <62415750+Yuviman@users.noreply.github.com>
2025-11-01 09:18:30 +0530

    Add training script for sequence classification model

--- week-5/code/detector_train.py
+++ week-5/code/detector_train.py
@@ -1,1 +1,16 @@
+from datasets import Dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+# preparing datasets
+train_ds = Dataset.from_pandas(train_df)  
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+def preprocess(batch):
+    return tokenizer(batch["text"], truncation=True, padding="max_length")
+train_ds = train_ds.map(preprocess, batched=True)
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+
+args = TrainingArguments("detector-checkpoint", per_device_train_batch_size=8, num_train_epochs=2, logging_steps=10)
+trainer = Trainer(model, args, train_dataset=train_ds)
+trainer.train()
+trainer.save_model("prompt_detector")
 
