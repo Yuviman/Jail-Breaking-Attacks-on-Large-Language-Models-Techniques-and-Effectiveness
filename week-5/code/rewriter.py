98567a84f59d3378ca19fab05cdd5d5fda2d9a28
Yuviman <62415750+Yuviman@users.noreply.github.com>
2025-11-01 09:06:38 +0530

    Add prompt rewriting functionality with sanitization

--- week-5/code/rewriter.py
+++ week-5/code/rewriter.py
@@ -1,1 +1,8 @@
+import re
+from code.sanitizer import sanitize_prompt
+
+def rewrite_prompt(text):
+    s = sanitize_prompt(text)
+    s = re.sub(r'(?i)(do anything now|be DAN|you are DAN|act as DAN)', 'please provide a factual answer about', s)
+    return s
 
